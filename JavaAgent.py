6c21e9db8246381cf4dd988555668c42f02b9186
Tyler Arehart <tarehart@gmail.com>
2017-08-24 21:40:45 -0700

    Updating the JavaAgent with a best effort to avoid port conflicts during tournaments.

--- JavaAgent.py
+++ JavaAgent.py
@@ -5,8 +5,11 @@ from py4j.java_gateway import GatewayParameters
 # In order to run this successfully:
 # 1. Run 'pip install py4j'  (one time setup)
 # 2. Modify runner.py to use this agent, e.g. "agent2 = JavaAgent.agent("orange")"
-# 3. Run the AgentEntryPoint class, and leave it running
-# 4. Run runner.py
+# 3. Choose a port number, and put it both in this class and in AgentEntryPoint.java
+#    - Avoid 25333 because that's the default, and other bots may be using it!
+#    - See the myPort variable below, which I've set to -1 so that you're forced to see this.
+# 4. Run the AgentEntryPoint class, and leave it running
+# 5. Run runner.py
 
 '''
 Hi! You can use this code as a template to create your own bot.  Also if you don't mind writing a blurb
@@ -21,7 +24,26 @@ debug any runtime issues that occur with your bot.
 # Can this bot's code be shared publicly (Default: No):
 # Can non-tournment gameplay of this bot be displayed publicly (Default: No):
 
-gateway = JavaGateway(gateway_parameters=GatewayParameters(auto_convert=True))
+myPort = -1  # -1 fails on purpose, see instructions above to change it!
+if myPort < 0:
+	# Feel free to remove this once you've chosen a port!
+	raise Exception('The person programming this bot (you?) needs to choose a port number! See instructions in this file')
+
+
+# Scenario: you finished your bot and submitted it to a tournament. Your opponent hard-coded the same
+# as you, and the match can't start because of the conflict. Because of this line, you can ask the
+# organizer make a file called "port.txt" in the same directory as your .jar, and put some other number in it.
+# This matches code in AgentEntryPoint.java
+try:
+	with open("port.txt", "r") as portFile:
+		myPort = int(portFile.readline())
+except ValueError:
+	print("Failed to parse port file! Will proceed with hard-coded port number.")
+except:
+	pass
+
+print("Connecting to Java Gateway on port " + str(myPort))
+gateway = JavaGateway(gateway_parameters=GatewayParameters(auto_convert=True, port=myPort))
 javaAgent = gateway.entry_point.getAgent()
 
 class agent:
--- java/src/rlbot/AgentEntryPoint.java
+++ java/src/rlbot/AgentEntryPoint.java
@@ -2,8 +2,19 @@ package rlbot;
 
 import py4j.GatewayServer;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * See JavaAgent.py for usage instructions
+ */
 public class AgentEntryPoint {
 
+    // TODO: You need to choose a unique port number for yourself that is unlikely to conflict with other bots! See JavaAgent.py for more details.
+    // This should match the port defined in JavaAgent.py
+    public static final int PORT = -1;
     private Agent agent;
 
     public AgentEntryPoint() {
@@ -15,9 +26,34 @@ public class AgentEntryPoint {
     }
 
     public static void main(String[] args) {
-        GatewayServer gatewayServer = new GatewayServer(new AgentEntryPoint());
+
+        // TODO: remove this once you've chosen a port
+        if (PORT < 0) {
+            throw new RuntimeException("The person programming this bot (you?) needs to choose a port number! See instructions in JavaAgent.py.");
+        }
+
+        // Scenario: you finished your bot and submitted it to a tournament. Your opponent hard-coded the same
+        // as you, and the match can't start because of the conflict. Because of this line, you can ask the
+        // organizer make a file called "port.txt" in the same directory as your .jar, and put some other number in it.
+        // This matches code in JavaAgent.py
+        int port = readPortFromFile().orElse(PORT);
+
+        GatewayServer gatewayServer = new GatewayServer(new AgentEntryPoint(), port);
         gatewayServer.start();
-        System.out.println("Gateway server started. Listening for Rocket League data!");
+        System.out.println(String.format("Gateway server started on port %s. Listening for Rocket League data!", port));
+    }
+
+    private static Optional<Integer> readPortFromFile() {
+        try {
+            Stream<String> lines = Files.lines(Paths.get("port.txt"));
+            Optional<String> firstLine = lines.findFirst();
+            return firstLine.map(Integer::parseInt);
+        } catch (NumberFormatException e) {
+            System.out.println("Failed to parse port file! Will proceed with hard-coded port number.");
+            return Optional.empty();
+        } catch (Throwable e) {
+            return Optional.empty();
+        }
     }
 
 }
