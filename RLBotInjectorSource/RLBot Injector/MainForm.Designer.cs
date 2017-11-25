3572632d4165b61df43191e31bc9786f44b41530
Douglas Sexton <sextondouglas@comcast.net>
2017-11-25 11:33:52 -0800

    Adding injector executable and source

---
+++ RLBotInjectorSource/RLBot Injector/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RLBot_Injector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.injectorTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
+            this.logoPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(328, 315);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 0;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(12, 330);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(329, 44);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Waiting for Rocket League...";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // injectorTimer
+            // 
+            this.injectorTimer.Tick += new System.EventHandler(this.injectorTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(353, 383);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.logoPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "RLBot Injector";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Timer injectorTimer;
+    }
+}
+
---
+++ RLBotInjectorSource/RLBot Injector/MainForm.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Media;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RLBot_Injector
+{
+    public partial class MainForm : Form
+    {
+        private enum ExitCodes
+        {
+            INJECTION_SUCCESSFUL,
+            INJECTION_FAILED,
+            MULTIPLE_ROCKET_LEAGUE_PROCESSES_FOUND,
+            RLBOT_DLL_ALREADY_INJECTED,
+            RLBOT_DLL_NOT_FOUND,
+            MULTIPLE_RLBOT_DLL_FILES_FOUND
+        }
+
+        string dllPath = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] dllPaths = Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "RLBot*.dll");
+
+            if (dllPaths.Length == 0)
+            {
+                MessageBox.Show("The RLBot Dll could not be found in the startup directory of this injector!",
+                            "RLBot Dll not found",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation);
+
+                Environment.Exit((int)ExitCodes.RLBOT_DLL_NOT_FOUND);
+            }
+            else if (dllPaths.Length > 1)
+            {
+                MessageBox.Show("Multiple RLBot Dll files have been found in the startup directory of this injector!\n" +
+                            "Please make sure that only one RLBot Dll file exists in that directory.",
+                            "Multiple RLBot Dll files found",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation);
+
+                Environment.Exit((int)ExitCodes.MULTIPLE_RLBOT_DLL_FILES_FOUND);
+            }
+
+            dllPath = dllPaths[0];
+            injectorTimer.Start();
+        }
+
+        private void injectorTimer_Tick(object sender, EventArgs e)
+        {
+            Process[] foundProcesses = Process.GetProcessesByName("RocketLeague");
+
+            if (foundProcesses.Length == 1)
+            {
+                injectorTimer.Stop();
+                statusLabel.Text = "Injecting...";
+                statusLabel.Update();
+
+                Process rlProcess = foundProcesses[0];
+
+                foreach (ProcessModule module in rlProcess.Modules)
+                {
+                    if (Regex.IsMatch(module.ModuleName, "RLBot.*.dll"))
+                    {
+                        MessageBox.Show("The RLBot Dll has already been injected into Rocket League!\n" +
+                            "Injecting it more than once is not possible.",
+                            "RLBot Dll already injected",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Exclamation);
+
+                        Environment.Exit((int)ExitCodes.RLBOT_DLL_ALREADY_INJECTED);
+                    }
+                }
+
+                if (Injector.Inject(rlProcess.Id, dllPath))
+                {
+                    statusLabel.Text = "Injection successful!";
+                    statusLabel.Update();
+                    (new SoundPlayer(Properties.Resources.Activated)).PlaySync();
+                    Environment.Exit((int)ExitCodes.INJECTION_SUCCESSFUL);
+                }
+                else
+                {
+                    statusLabel.Text = "Injection failed!";
+                    statusLabel.Update();
+                    (new SoundPlayer(Properties.Resources.FailedToInjectTheDll)).PlaySync();
+                    Environment.Exit((int)ExitCodes.INJECTION_FAILED);
+                }
+            }
+            else if (foundProcesses.Length > 1)
+            {
+                injectorTimer.Stop();
+
+                MessageBox.Show("Multiple Rocket League processes have been found!\n" +
+                    "Please make sure that only one instance of Rocket League is running before starting this injector.",
+                    "Multiple Rocket League instances found",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+
+                Environment.Exit((int)ExitCodes.MULTIPLE_ROCKET_LEAGUE_PROCESSES_FOUND);
+            }
+        }
+    }
+}
