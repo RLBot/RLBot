0cdb85330375100975d0aaab00c8cee319966ec0
Tyler Arehart <tarehart@gmail.com>
2018-01-28 16:54:33 -0800

    Improving grpc behavior, documenation, and sample code.

--- grpcsupport/README.md
+++ grpcsupport/README.md
@@ -1,7 +1,15 @@
 Read this to understand what's going on with grpc / protobuf: https://grpc.io/docs/quickstart/python.html
 
-To generate the game_data_pb2* files inside the protobuf folder I run this:
+
+To run a bot that uses grpc, you need to have the python module installed:
 ```
+pip install grpcio
+```
+
+If you need to make changes to the API and regenerate the game_data_pb2* files inside the protobuf folder, run this:
+```
+pip install grpcio-tools
+
 cd <one directory above this>
 python -m grpc_tools.protoc -I./ --python_out=./ --grpc_python_out=./ grpcsupport/protobuf/game_data.proto
 ```
--- grpcsupport/grpc_demo_agent.py
+++ grpcsupport/grpc_demo_agent.py
@@ -4,7 +4,9 @@ import grpc
 import time
 
 ####################################################################################################
-# To run this agent successfully, you need to have grpc_demo_server.py running at the same time!
+# To run this agent successfully, you need to:
+# - install the python package "grpcio", e.g. pip install grpcio
+# - have grpc_demo_server.py running at the same time!
 ####################################################################################################
 
 class Agent:
@@ -13,20 +15,21 @@ class Agent:
         self.team = team  # use self.team to determine what team you are. I will set to "blue" or "orange"
         self.index = index
         self.stub = None
-        self.myPort = '34865'
+        self.myPort = '34865' # Choose a different port for your own bot so it doesnt conflict in tournaments.
+        self.connected = False
 
         try:
             self.init_protobuf()
         except Exception as e:
-            print("Exception when trying to connect to protobuf server: " + str(e))
+            print("Exception when trying to connect to grpc server: " + str(e))
             pass
 
     def init_protobuf(self):
-        print("Connecting to protobuf server on port " + self.myPort + '...')
+        print("Connecting to grpc server on port " + self.myPort + '...')
         channel = grpc.insecure_channel('localhost:' + self.myPort)
         self.stub = game_data_pb2_grpc.BotStub(channel)
-        grpc.channel_ready_future(channel).result()
-        print("Connection to server successful!")
+        # grpc.channel_ready_future(channel).result()
+        # print("Connection to server successful!")
 
     def get_output_vector(self, game_tick_packet):
 
@@ -34,6 +37,11 @@ class Agent:
 
         try:
             controller_state = self.stub.GetControllerState(proto)
+
+            if (not self.connected):
+                print("Connected to grpc server successfully!")
+                self.connected = True
+
             return [
                 controller_state.throttle,
                 controller_state.steer,
@@ -45,13 +53,9 @@ class Agent:
                 controller_state.handbrake
             ]
         except Exception as e:
-            print("Exception when calling protobuf server: " + str(e))
-            print("Will recreate connection in a few seconds...")
+            print("Exception when calling grpc server: " + str(e))
+            print("Will try again in a few seconds...")
+            self.connected = False
             time.sleep(4)
-            try:
-                self.init_protobuf()
-            except Exception as e:
-                print("Reinitialization failed: " + str(e))
-                pass
 
             return [0, 0, 0, 0, 0, 0, 0, 0]  # No motion
