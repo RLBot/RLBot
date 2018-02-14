ffbf822013343bf9daf6c8f9b64282e444b30a94
Tyler Arehart <tarehart@gmail.com>
2018-02-13 21:05:40 -0800

    Getting the grpc agent working in v4.

--- grpcsupport/grpc_client.py
+++ grpcsupport/grpc_client.py
@@ -2,9 +2,11 @@ import grpc
 import time
 from . import proto_converter
 from .protobuf import game_data_pb2_grpc
+from agents.base_agent import BaseAgent
+
 
 def make_grpc_agent(server_address):
-    '''
+    """
         parameters:
             server_address - grpc target string. example: 'localhost:34865'
         returns:
@@ -12,28 +14,23 @@ def make_grpc_agent(server_address):
             grpc server and returns its response.
 
         For an example use of this, see: example_bots/grpc_demo_agent
-    '''
+    """
 
-    class GrpcForwardingAgent:
+    class GrpcForwardingAgent(BaseAgent):
         def __init__(self, name, team, index):
-            self.name = name
-            self.team = team  # use self.team to determine what team you are. I will set to "blue" or "orange"
-            self.index = index
+            super().__init__(name, team, index)
             self.stub = None
             self.connected = False
+            self.init_protobuf()
 
+        def init_protobuf(self):
             try:
-                self.init_protobuf()
+                print("Connecting to grpc server: " + server_address)
+                channel = grpc.insecure_channel(server_address)
+                self.stub = game_data_pb2_grpc.BotStub(channel)
             except Exception as e:
                 print("Exception when trying to connect to grpc server: " + str(e))
                 pass
-
-        def init_protobuf(self):
-            print("Connecting to grpc server: " + server_address)
-            channel = grpc.insecure_channel(server_address)
-            self.stub = game_data_pb2_grpc.BotStub(channel)
-            # grpc.channel_ready_future(channel).result()
-            # print("Connection to server successful!")
 
         def get_output_vector(self, game_tick_packet):
 
@@ -42,7 +39,7 @@ def make_grpc_agent(server_address):
             try:
                 controller_state = self.stub.GetControllerState(proto)
 
-                if (not self.connected):
+                if not self.connected:
                     print("Connected to grpc server successfully!")
                     self.connected = True
 
@@ -63,4 +60,5 @@ def make_grpc_agent(server_address):
                 time.sleep(4)
 
                 return [0, 0, 0, 0, 0, 0, 0, 0]  # No motion
+
     return GrpcForwardingAgent
