1ffc07d0316462bc01126752f2d9269808c80c57
dtracers <dtracers@gmail.com>
2018-02-17 15:14:21 -0800

    load a match function

--- gui/match_settings_frame.py
+++ gui/match_settings_frame.py
@@ -6,3 +6,5 @@ class SettingsFrame(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, *kwargs)
         ttk.Label(self, text="Match settings").pack()
+
+    def load_match_settings(self, config_file):
