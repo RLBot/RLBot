242daba0ab98068728bd08e6a06a50fdf51e40af
dtracers <dtracers@gmail.com>
2018-02-17 16:14:00 -0800

    creates a custom frame for detailed bot options

--- gui/custom_agent_frame.py
+++ gui/custom_agent_frame.py
@@ -8,24 +8,24 @@ class CustomAgentFrame(tk.Frame):
     custom_agent_options = None
     bot_config = None
 
-    def __init__(self, parent, agent_module, *args, **kwargs):
+    def __init__(self, parent, agent_module, bot_config, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, *kwargs)
         self.agent_module = agent_module
+        self.custom_agent_options = tk.Frame(self, borderwidth=2, relief=tk.SUNKEN)
         if agent_module is None:
-            self.agent_module = BaseAgent
+            self.agent_module = [None, BaseAgent]
+        self.bot_config = bot_config
 
     def initialise_custom_config(self):
         """Create the Custom Config Frame containing all widgets for editing the parameters."""
+        if not self.custom_agent_options.winfo_ismapped():
+            self.custom_agent_options.grid(row=40, column=0, columnspan=3, sticky="nsew")
         for widget in self.custom_agent_options.grid_slaves():
             widget.grid_forget()
-        try:
-            self.bot_config = self.agent_module.create_agent_configurations()
-        except AttributeError:
-            error = "This class does not contain a config method, unable to create custom config"
-            ttk.Label(self.custom_agent_options, text=error).grid()
-            return
+        if self.bot_config is None:
+            self.bot_config = self.agent_module[1].create_agent_configurations()
 
-        if not self.bot_config.values:
+        if self.bot_config is None:
             ttk.Label(self.custom_agent_options, text="No Bot Parameters for this agent").grid()
             return
 
@@ -66,3 +66,5 @@ class CustomAgentFrame(tk.Frame):
                     parameter.value.set(parameter.default)
                 total_count += 1
         self.custom_agent_options.grid_columnconfigure(1, weight=1)
+
+        return self.bot_config
