4dc5a1e7bb458a30555cec27ce5b80ccd009ac75
dtracers <dtracers@gmail.com>
2018-02-17 16:12:45 -0800

    added a way to get the base repo path

--- gui/utils.py
+++ gui/utils.py
@@ -6,6 +6,11 @@ import os
 def get_file(filetypes=None, title=None):
     """Grabs a file always opening at the base directory of this repo."""
     return askopenfilename(
-        initialdir=os.path.dirname(os.path.realpath(__file__)),
+        initialdir=get_base_repo_path(),
         filetypes=filetypes,
         title=title)
+
+
+def get_base_repo_path():
+    """Gets the path of the RLBot directory"""
+    return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
