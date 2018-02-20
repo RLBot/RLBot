021a3ee4942f6a3f72da65c8afca2d3c55120d19
dtracers <dtracers@gmail.com>
2018-02-20 10:12:10 -0800

    a class for managing indexes between teams

--- gui/utils.py
+++ gui/utils.py
@@ -14,3 +14,19 @@ def get_file(filetypes=None, title=None):
 def get_base_repo_path():
     """Gets the path of the RLBot directory"""
     return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+class IndexManager:
+    def get_new_index(self):
+        """
+        Gets the next available index and marks it as in use.
+        :return:  An index that is not currently in use
+        """
+        return -1
+
+    def free_index(self, index):
+        """
+        Tells this manager that the passed in index is now free to reuse.
+        :param index: The index that is now free to reuse.
+        """
+        pass
