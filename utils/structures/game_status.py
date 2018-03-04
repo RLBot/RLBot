e2a3fbb9700d8cecdf73f4b05b441154671fb6b5
dtracers <dtracers@gmail.com>
2018-03-04 10:34:09 -0800

    added a status class to hold all the game statuses

---
+++ utils/structures/game_status.py
@@ -0,0 +1,26 @@
+def create_status_object():
+    status_list = ["Success",
+     "CoreNotRunning",
+     "InvalidNumPlayers",
+     "InvalidBotSkill",
+     "InvalidHumanIndex",
+     "InvalidName",
+     "InvalidTeam",
+     "InvalidTeamColorID",
+     "InvalidCustomColorID",
+     "InvalidGameValues",
+     "InvalidThrottle",
+     "InvalidSteer",
+     "InvalidPitch",
+     "InvalidYaw",
+     "InvalidRoll",
+     "InvalidPlayerIndex",
+     "InvalidQuickChatPreset"]
+    result = lambda: None
+    for i in range(len(status_list)):
+        setattr(result, status_list[i], i)
+    setattr(result, 'status_list', status_list)
+    return result
+
+
+RLBotCoreStatus = create_status_object()
