5968afcf69f2c709305268fd01af49a67296b564
dtracers <dtracers@gmail.com>
2018-03-14 21:53:14 -0700

    added a default logger

--- RLBotFramework/utils/logging_utils.py
+++ RLBotFramework/utils/logging_utils.py
@@ -1,12 +1,14 @@
 import inspect
 import logging
 
+
+DEFAULT_LOGGER = 'rlbot'
 logger_initialized = False
 logging_level = logging.DEBUG
 FORMAT = "%(levelname)s:%(name)5s[%(filename)20s:%(lineno)s - %(funcName)20s() ] %(message)s"
 
 
-def get_logger(logger_name):
+def get_logger(logger_name, log_creation=True):
     global logger_initialized
     if not logger_initialized:
         logging.basicConfig(format=FORMAT, level=logging_level)
@@ -14,10 +16,15 @@ def get_logger(logger_name):
         logger_initialized = True
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging_level)
-
-    curframe = inspect.currentframe()
-    calframe = inspect.getouterframes(curframe, 2)
-    parent_call = calframe[1]
-    file_name = parent_call.filename
-    logger.debug('creating logger for %s', file_name)
+    if log_creation:
+        curframe = inspect.currentframe()
+        calframe = inspect.getouterframes(curframe, 2)
+        parent_call = calframe[1]
+        file_name = parent_call.filename
+        logger.debug('creating logger for %s', file_name)
     return logger
+
+
+def log_warn(message, args):
+    """Logs a warning message using the default logger."""
+    get_logger(DEFAULT_LOGGER, log_creation=False).log(level=logging.WARNING, msg=message, args=args)
