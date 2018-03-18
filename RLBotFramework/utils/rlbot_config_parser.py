d950edf026976278f94e56f1f241b0a64620e750
hallo-doei <edwin.kanis@outlook.com>
2018-03-18 20:20:03 +0100

    added mutators and other match settings to rlbot config layout

--- RLBotFramework/utils/rlbot_config_parser.py
+++ RLBotFramework/utils/rlbot_config_parser.py
@@ -18,6 +18,25 @@ PARTICIPANT_BOT_SKILL_KEY = 'participant_bot_skill'
 PARTICIPANT_TEAM = 'participant_team'
 RLBOT_CONFIG_FILE = 'rlbot.cfg'
 RLBOT_CONFIGURATION_HEADER = 'RLBot Configuration'
+
+MUTATOR_CONFIGURATION_HEADER = "Mutator Configuration"
+MUTATOR_MATCH_LENGTH = "Match Length"
+MUTATOR_MAX_SCORE = "Max Score"
+MUTATOR_GAME_SPEED = "Game Speed"
+MUTATOR_BALL_MAX_SPEED = "Ball Max Speed"
+MUTATOR_BALL_TYPE = "Ball Type"
+MUTATOR_BALL_WEIGHT = "Ball Weight"
+MUTATOR_BALL_SIZE = "Ball Size"
+MUTATOR_BALL_BOUNCINESS = "Ball Bounciness"
+MUTATOR_BOOST_AMOUNT = "Boost Amount"
+MUTATOR_RUMBLE = "Rumble"
+MUTATOR_BOOST_STRENGTH = "Boost Strength"
+MUTATOR_GRAVITY = "Gravity"
+MUTATOR_DEMOLISH = "Demolish"
+MUTATOR_RESPAWN_TIME = "Respawn Time"
+
+TEAM_CONFIGURATION_HEADER = "Team Configuration"
+
 PARTICIPANT_COUNT_KEY = 'num_participants'
 EXTENSION_PATH_KEY = 'extension_path'
 
@@ -98,6 +117,54 @@ def create_bot_config_layout():
                                              ' this value will be used to set bot skill.\n' +
                                              '0.0 is Rookie, 0.5 is pro, 1.0 is all-star. ' +
                                              ' You can set values in-between as well.')
+
+    mutator_header = config_object.add_header_name(MUTATOR_CONFIGURATION_HEADER)
+    mutator_header.add_value(MUTATOR_MATCH_LENGTH, int, default=5,
+                             description="Changes the length of the match, 0 for unlimited")
+    mutator_header.add_value(MUTATOR_MAX_SCORE, int, default=0,
+                             description="Changes the number of goals needed to win, 0 for unlimited")
+    mutator_header.add_value(MUTATOR_GAME_SPEED, str, default="Default",
+                             description="'Default', 'Slo-Mo' or 'Time Warp'\n"
+                                         "    Slo-Mo slows the game down\n"
+                                         "    Time Warp only slows the game down when a player touches the ball")
+    mutator_header.add_value(MUTATOR_BALL_MAX_SPEED, int, default=0,  # TODO find real default max
+                             description="Sets max speed of the ball in km/h, 0 for unlimited.")
+    mutator_header.add_value(MUTATOR_BALL_TYPE, str, default="Default",
+                             description="Changes the type of the ball (use 'Default', 'Cube', 'Puck' or 'Basketball')")
+    mutator_header.add_value(MUTATOR_BALL_WEIGHT, str, default="Default",
+                             description="Changes the weight of the ball (use 'Default', 'Super Light', "
+                                         "'Light' or 'Heavy')")
+    mutator_header.add_value(MUTATOR_BALL_SIZE, float, default=1.0,  # TODO find real default size
+                             description="Changes the size of the ball")
+    mutator_header.add_value(MUTATOR_BALL_BOUNCINESS, float, default=1.0,  # TODO find real default
+                             description="Changes the bounciness of the ball")
+    mutator_header.add_value(MUTATOR_BOOST_AMOUNT, str, default="Default",
+                             description="Changes the amount of boost \n (use 'Default', 'Unlimited', "
+                                         "'Recharge (Slow)', 'Recharge (Fast)' or 'No Boost')")
+    mutator_header.add_value(MUTATOR_RUMBLE, str, default="None",
+                             description="Changes rumble type\n(use 'None', 'Default', "
+                                         "'Slow', 'Civilized', 'Desctuction Derby' or 'Spring Loaded'")
+    mutator_header.add_value(MUTATOR_BOOST_STRENGTH, float, default=1.0,
+                             description="Amount to multiply default boost strength with")
+    mutator_header.add_value(MUTATOR_GRAVITY, str, default="Default",
+                             description="Changes gravity for both cars and ball\n"
+                                         "(use 'Default', 'Low', 'High' or 'Super High'")
+    mutator_header.add_value(MUTATOR_DEMOLISH, str, default="Default",
+                             description="Changes demolishment sensitivity\n(use 'Default', 'Disabled', "
+                                         "'Friendly Fire', 'On Contact' or 'On Contact (FF)'")
+    mutator_header.add_value(MUTATOR_RESPAWN_TIME, float, default=3,
+                             description="Time in seconds taken to respawn for demolished players\n"
+                                         "use -1 to set Disable Goal Reset")
+
+    team_header = config_object.add_header_name(TEAM_CONFIGURATION_HEADER)
+    team_header.add_value("Team Blue Color", int, default=0,
+                          description="Changes Blue team color, use 0 to use default color")
+    team_header.add_value("Team Blue Name", str, default="Blue",
+                          description="Changes the Team name to use instead of 'Blue'")
+    team_header.add_value("Team Orange Color", int, default=0,
+                          description="Changes Blue team color, use 0 to use default color")
+    team_header.add_value("Team Orange Name", str, default="Blue",
+                          description="Changes the Team name to use instead of 'Orange'")
     return config_object
 
 
