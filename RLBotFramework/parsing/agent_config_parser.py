9f30cffc1a3c1a9a346e2dab884dcf3e1069b5dc
dtracers <dtracers@gmail.com>
2018-04-01 17:20:57 -0700

    changed names again

--- RLBotFramework/parsing/agent_config_parser.py
+++ RLBotFramework/parsing/agent_config_parser.py
@@ -49,7 +49,8 @@ def add_participant_header(config_object):
                                              human - not controlled by the framework
                                              rlbot - controlled by the framework
                                              psyonix - default bots (skill level can be changed with participant_bot_skill
-                                             possessed_human - controlled by the framework but the game detects it as a human""")
+                                             party_member_bot - controlled by an rlbot but the game detects it as a human
+                                             controller_passthrough - controlled by a human but runs through the framework""")
 
     participant_header.add_value(PARTICIPANT_BOT_SKILL_KEY, float, default=1.0,
                                  description='If participant is a bot and not RLBot controlled,' +
@@ -121,13 +122,13 @@ def get_bot_options(bot_type):
     elif bot_type == 'psyonix':
         is_bot = True
         is_rlbot = False
-    elif bot_type == 'human_bot':
+    elif bot_type == 'controller_passthrough':
         # this is an rlbot but a very specific rlbot
         is_bot = True
         is_rlbot = True
-    elif bot_type == 'bot_human':
+    elif bot_type == 'party_member_bot':
         # this is a bot running under a human
-        
+
         is_rlbot = True
         is_bot = False
     else:
