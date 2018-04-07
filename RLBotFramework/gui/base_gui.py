fa80cfb2ee3366a588fe31d518c7ea23ed4feb53
hallo-doei <edwin.kanis@outlook.com>
2018-04-07 15:26:27 +0200

    some preset stuff added

--- RLBotFramework/gui/base_gui.py
+++ RLBotFramework/gui/base_gui.py
@@ -1,11 +1,10 @@
 import os
 from PyQt5.QtCore import QTimer
 
-# from RLBotFramework.parsing.agent_config_parser import get_team
-from RLBotFramework.parsing.custom_config import ConfigObject
 from RLBotFramework.gui.index_manager import IndexManager
 from RLBotFramework.gui.base_gui_agent import BaseGuiAgent
 from RLBotFramework.parsing.rlbot_config_parser import create_bot_config_layout, get_num_players
+from RLBotFramework.gui.presets import LoadoutPreset, AgentPreset
 
 
 class BaseGui:
@@ -13,11 +12,8 @@ class BaseGui:
     Handles GUI functions that should be shared across all GUIs, such as:
         index_manager, overall_config, add_agent/remove_agent (with index_manager handled)
     """
-    overall_config = None
-    agents_frame = None
-    add_agent_object = None
 
-    index_manager = IndexManager
+    index_manager_class = IndexManager
     agent_class = BaseGuiAgent
 
     def __init__(self, overall_config=None):
@@ -25,25 +21,19 @@ class BaseGui:
         self.overall_config_path = None
         self.agent_class.overall_config = overall_config
 
-        self.index_manager = self.index_manager(10)
+        self.index_manager = self.index_manager_class(10)
+        self.loadout_presets = {}
+        self.agent_presets = {}
 
-        self.gui_config = self.create_gui_config()
-        self.latest_save_path = self.gui_config.get("GUI Configuration", "latest_save_path")
-        self.load_cfg(self.latest_save_path)
+        self.load_cfg("rlbot.cfg")
 
         self.agents = []
         self.load_agents()
 
-    def load_cfg(self, config_path, teams=False, match_settings=False):
+    def load_cfg(self, config_path):
         if self.overall_config is None:
             self.overall_config = create_bot_config_layout()
         self.overall_config.parse_file(config_path, 10)
-        if teams:
-            self.index_manager.numbers = set()
-            self.team1.load_agents(self.overall_config)
-            self.team2.load_agents(self.overall_config)
-        if match_settings:
-            self.match_settings.load_match_settings(self.overall_config)
         self.agent_class.overall_config = self.overall_config
 
     def save_overall_config(self):
@@ -66,12 +56,17 @@ class BaseGui:
             self.overall_config = config_file
         num_participants = get_num_players(self.overall_config)
         for i in range(num_participants):
-            self.add_agent(overall_index=i)
+            agent = self.add_agent(overall_index=i)
+            loadout_preset = self.add_loadout_preset(agent.get_agent_config_path())
+            agent.loadout_preset = loadout_preset.get_name()
+            agent_preset = self.add_agent_preset(agent.get_agent_config_path())
+            agent.agent_preset = agent_preset.get_name()
 
     def add_agent(self, overall_index=None, team_index=None):
         """
         Creates the agent using self.agent_class and adds it to the index manager.
         :param overall_index: The index of the bot in the config file if it already exists.
+        :param team_index: The index of the team to place the agent in
         :return agent:
         """
         if not self.index_manager.has_free_slots():
@@ -82,9 +77,19 @@ class BaseGui:
             self.index_manager.use_index(overall_index)
         agent = self.agent_class(overall_index=overall_index, team_i=team_index)
         self.agents.append(agent)
-
         return agent
 
+    def add_loadout_preset(self, file_path):
+        preset = LoadoutPreset(file_path)
+        print(preset.get_name())
+        self.loadout_presets[preset.get_name()] = preset
+        return preset
+
+    def add_agent_preset(self, file_path):
+        preset = AgentPreset(file_path)
+        preset_name = os.path.basename(file_path)
+        self.agent_presets[preset_name] = preset
+        return preset
 
     def remove_agent(self, agent):
         """
@@ -103,15 +108,5 @@ class BaseGui:
         raise NotImplementedError('Subclasses of BaseGui must override this.')
 
     @staticmethod
-    def create_gui_config():
-        config = ConfigObject()
-        config.add_header_name("GUI Configuration") \
-            .add_value("blue_team_type", str, default="default") \
-            .add_value("orange_team_type", str, default="default") \
-            .add_value("agent_type", str, default="default") \
-            .add_value("latest_save_path", str, default="rlbot.cfg")
-        return config
-
-
-    def main(self):
+    def main():
         raise NotImplementedError('Subclasses of BaseGui must override this.')
