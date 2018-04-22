03bbe8b3f25b73d11d6aa28a26e458afc9757f58
Tyler Arehart <tarehart@gmail.com>
2018-04-22 04:31:45 -0700

    Un-ignoring capnp generation exes in project. Deleting them from lib.

--- RLBot Core Messages/CapnProto/capnproto.hpp
+++ RLBot Core Messages/CapnProto/capnproto.hpp
@@ -1,12 +1,13 @@
 #ifndef CAPN_CONVERSIONS_HPP
 #define CAPN_CONVERSIONS_HPP
 
+// The order of include statements here is delicate. If you mess with it, don't be surprised if you get weird compile errors.
 
 #include "..\PacketStructs\LiveDataPacket.hpp"
 #include "..\PacketStructs\PacketStructs.hpp"
 #include "..\ErrorCodes\ErrorCodes.hpp"
 #include <kj\windows-sanity.h>
-#undef VOID
+#undef VOID // One of the windows headers defines this and it screws up capnproto.
 #include "game_data.capnp.h"
 #include <capnp\serialize.h>
 #include <capnp\message.h>
