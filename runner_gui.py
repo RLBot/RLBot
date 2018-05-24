92084ae8ca04cc45d0479b7b3a35ce941f76dbea
Tyler Arehart <tarehart@gmail.com>
2018-05-23 20:46:02 -0700

    Fixing merge errors.

--- runner_gui.py
+++ runner_gui.py
@@ -1,7 +1,7 @@
 import sys
 import os
-sys.path.insert(0, os.path.realpath(os.path.dirname(__file__) + '/src/main/python/'))
-from src.main.python.RLBotFramework.gui.qt_root import RLBotQTGui
+sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.abspath(__file__)) + '/src/main/python/'))
+from RLBotFramework.gui.qt_root import RLBotQTGui
 
 if __name__ == '__main__':
     RLBotQTGui.main()
