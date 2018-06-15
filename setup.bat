534259246677b35bf62c0e3897b301b3aee8859f
Redox <exploreblocks@gmail.com>
2018-06-15 16:56:19 +0200

    Added C# Flatbuffers generation to setup.bat

--- setup.bat
+++ setup.bat
@@ -13,6 +13,9 @@ xcopy /s /Y .\src\generated\python\rlbot .\src\main\python\rlbot\messages\
 
 .\src\main\flatbuffers\flatc.exe --java -o .\src\generated\java\flatbuffers .\src\main\flatbuffers\rlbot.fbs
 
+.\src\main\flatbuffers\flatc.exe --csharp -o .\src\generated\cs\flatbuffers .\src\main\flatbuffers\rlbot.fbs
+xcopy /s /Y .\src\generated\cs\flatbuffers\rlbot\flat .\src\main\cs\RLBotDotNet\RLBotDotNet\flat
+
 echo.
 echo #######################
 echo ### Setup Complete! ###
