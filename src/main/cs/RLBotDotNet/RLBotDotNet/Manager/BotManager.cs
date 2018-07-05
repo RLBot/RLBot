776c2b43891b41698666fd836d9cdb190874c169
alion02 <alojczek@gmail.com>
2018-07-05 19:13:28 +0200

    Pulled TimerResolutionInterop into its own file in the Utils namespace

--- src/main/cs/RLBotDotNet/RLBotDotNet/Manager/BotManager.cs
+++ src/main/cs/RLBotDotNet/RLBotDotNet/Manager/BotManager.cs
@@ -3,7 +3,6 @@ using RLBotDotNet.Utils;
 using RLBotDotNet.Server;
 using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Threading;
 using System.Linq;
@@ -11,20 +10,6 @@ using System.IO;
 
 namespace RLBotDotNet
 {
-    class TimerResolutionInterop
-    {
-        [DllImport("ntdll.dll", SetLastError = true)]
-        static extern int NtSetTimerResolution(int DesiredResolution, bool SetResolution, out int CurrentResolution);
-        public static void SetResolution(int desiredResolution) => NtSetTimerResolution(desiredResolution, true, out _);
-
-        [DllImport("ntdll.dll", SetLastError = true)]
-        static extern int NtQueryTimerResolution(out int MinimumResolution, out int MaximumResolution, out int CurrentResolution);
-        public static void Query(out int minResolution, out int maxResolution, out int currResolution) => NtQueryTimerResolution(out minResolution, out maxResolution, out currResolution);
-        public static TimeSpan MinimumResolution { get { NtQueryTimerResolution(out int minRes, out _, out _); return new TimeSpan(minRes); } }
-        public static TimeSpan MaximumResolution { get { NtQueryTimerResolution(out _, out int maxRes, out _); return new TimeSpan(maxRes); } }
-        public static TimeSpan CurrentResolution { get { NtQueryTimerResolution(out _, out _, out int currRes); return new TimeSpan(currRes); } }
-    }
-
     /// <summary>
     /// Manages the C# bots and runs them.
     /// </summary>
