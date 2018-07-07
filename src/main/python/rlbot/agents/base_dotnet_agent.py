83da1455a8104660bece8edeaee0db29222048df
Tyler Arehart <tarehart@gmail.com>
2018-07-06 19:49:45 -0700

    Implementing get_extra_pids in base_dotnet_agent for fairness.

--- src/main/python/rlbot/agents/base_dotnet_agent.py
+++ src/main/python/rlbot/agents/base_dotnet_agent.py
@@ -1,8 +1,10 @@
+import msvcrt
 import socket
 import time
 
+import psutil
+
 from rlbot.agents.base_independent_agent import BaseIndependentAgent
-from rlbot.utils.logging_utils import get_logger
 from rlbot.utils.structures import game_interface
 
 
@@ -12,16 +14,16 @@ class BaseDotNetAgent(BaseIndependentAgent):
         self.name = name
         self.team = team
         self.index = index
+        self.port = self.read_port_from_file()
 
     def run_independently(self, terminate_request_event):
 
         while not terminate_request_event.is_set():
-            port = self.read_port_from_file()
             message = "add {0} {1} {2} {3}".format(self.name, self.team, self.index, game_interface.get_dll_directory())
 
             try:
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(("127.0.0.1", port))
+                s.connect(("127.0.0.1", self.port))
                 s.send(bytes(message, "ASCII"))
                 s.close()
             except ConnectionRefusedError:
@@ -30,6 +32,21 @@ class BaseDotNetAgent(BaseIndependentAgent):
             time.sleep(1)
         else:
             self.retire()
+
+    def get_extra_pids(self):
+        """
+        Gets the list of process ids that should be marked as high priority.
+        :return: A list of process ids that are used by this bot in addition to the ones inside the python process.
+        """
+        while True:
+            if msvcrt.kbhit():
+                return []
+            for proc in psutil.process_iter():
+                for conn in proc.connections():
+                    if conn.laddr.port == self.port:
+                        self.logger.debug('.NET socket server for {} appears to have pid {}'.format(self.name, proc.pid))
+                        return [proc.pid]
+            time.sleep(1)
 
     def retire(self):
         port = self.read_port_from_file()
