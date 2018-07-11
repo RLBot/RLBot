b6713791ddd7dde094584577871c56a3c72ac7cc
dtracers <dtracers@gmail.com>
2018-07-10 21:36:53 -0600

    Update quick_chats.py

--- src/main/python/rlbot/utils/structures/quick_chats.py
+++ src/main/python/rlbot/utils/structures/quick_chats.py
@@ -10,49 +10,8 @@ from rlbot.utils.logging_utils import get_logger
 
 def get_quick_chats():
     """
-    quick_chat_list = ["Information_IGotIt",
-                       "Information_NeedBoost",
-                       "Information_TakeTheShot",
-                       "Information_Defending",
-                       "Information_GoForIt",
-                       "Information_Centering",
-                       "Information_AllYours",
-                       "Information_InPosition",
-                       "Information_Incoming",
-                       "Compliments_NiceShot",
-                       "Compliments_GreatPass",
-                       "Compliments_Thanks",
-                       "Compliments_WhatASave",
-                       "Compliments_NiceOne",
-                       "Compliments_WhatAPlay",
-                       "Compliments_GreatClear",
-                       "Compliments_NiceBlock",
-                       "Reactions_OMG",
-                       "Reactions_Noooo",
-                       "Reactions_Wow",
-                       "Reactions_CloseOne",
-                       "Reactions_NoWay",
-                       "Reactions_HolyCow",
-                       "Reactions_Whew",
-                       "Reactions_Siiiick",
-                       "Reactions_Calculated",
-                       "Reactions_Savage",
-                       "Reactions_Okay",
-                       "Apologies_Cursing",
-                       "Apologies_NoProblem",
-                       "Apologies_Whoops",
-                       "Apologies_Sorry",
-                       "Apologies_MyBad",
-                       "Apologies_Oops",
-                       "Apologies_MyFault",
-                       "PostGame_Gg",
-                       "PostGame_WellPlayed",
-                       "PostGame_ThatWasFun",
-                       "PostGame_Rematch",
-                       "PostGame_OneMoreGame",
-                       "PostGame_WhatAGame",
-                       "PostGame_NiceMoves",
-                       "PostGame_EverybodyDance"]
+    Look for quick chats from here:
+    https://github.com/RLBot/RLBot/blob/master/src/main/flatbuffers/rlbot.fbs
     """
     result = lambda: None
     obj = QuickChatSelection.QuickChatSelection
