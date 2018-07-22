43c05b9c2b31c4d309452de9b929b109ac6d65b6
dtracers <dtracers@gmail.com>
2018-07-22 11:03:34 -0600

    calls exit to main menu

--- src/test/python/extensions/simple_extension.py
+++ src/test/python/extensions/simple_extension.py
@@ -3,8 +3,8 @@ from rlbot.utils.logging_utils import get_logger
 
 
 class SimpleExtension(BaseExtension):
-    def __init__(self, setup_manager):
-        super().__init__(setup_manager)
+    def __init__(self, setup_manager, game_interface):
+        super().__init__(setup_manager, game_interface)
         self.logger = get_logger("SimpleExtension")
 
     def on_match_start(self):
@@ -12,4 +12,5 @@ class SimpleExtension(BaseExtension):
 
     def on_match_end(self, score, scoreboard):
         self.logger.error("SCORE IS: %s", str(score))
+        self.game_interface.exit_to_menu()
 
