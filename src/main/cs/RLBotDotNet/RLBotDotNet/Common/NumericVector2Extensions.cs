a5cb2568323312edefade271d781a34489a1aa6a
Whok <whok1988@gmail.com>
2018-08-06 16:55:25 -0400

    Added support for Rendering drawings on the screen in the C# library.

---
+++ src/main/cs/RLBotDotNet/RLBotDotNet/Common/NumericVector2Extensions.cs
@@ -0,0 +1,22 @@
+﻿using FlatBuffers;
+using rlbot.flat;
+
+namespace RLBotDotNet.Common
+{
+    /// <summary>
+    /// Extension methods for <see cref="System.Numerics.Vector2"/>.
+    /// </summary>
+    public static class NumericVector2Extensions
+    {
+        /// <summary>
+        /// Converts the <see cref="System.Numerics.Vector2"/> to offset vector with the given <see cref="FlatBufferBuilder"/>.
+        /// </summary>
+        /// <param name="vector2">The <see cref="System.Numerics.Vector2"/> to convert.</param>
+        /// <param name="builder">The <see cref="FlatBufferBuilder"/> to use to create the offset vector.</param>
+        /// <returns>The created offset vector from the <see cref="System.Numerics.Vector2"/>.</returns>
+        public static Offset<Vector3> ToOffsetVector(this System.Numerics.Vector2 vector2, FlatBufferBuilder builder)
+        {
+            return Vector3.CreateVector3(builder, vector2.X, vector2.Y, 0);
+        }
+    }
+}
---
+++ src/main/cs/RLBotDotNet/RLBotDotNet/Renderer/NamedRenderer.cs
@@ -0,0 +1,39 @@
+﻿using FlatBuffers;
+
+namespace RLBotDotNet.Renderer
+{
+    /// <summary>
+    /// Renderer that uses a name as its index.
+    /// </summary>
+    public class NamedRenderer : Renderer
+    {
+        /// <summary>
+        /// Constructs a new instance of NamedRenderer using the render name as the index.
+        /// </summary>
+        /// <param name="renderName">The name to use as the render index.</param>
+        public NamedRenderer(string renderName) : base(renderName.GetHashCode())
+        {
+        }
+
+        /// <summary>
+        /// Starts the render packet.
+        /// </summary>
+        public void StartPacket()
+        {
+            Builder = new FlatBufferBuilder(1000);
+        }
+
+        /// <summary>
+        /// Finishes and sends the render packet.
+        /// </summary>
+        public void FinishAndSend()
+        {
+            SendPacket(FinishPacket());
+        }
+
+        private RenderPacket FinishPacket()
+        {
+            return DoFinishPacket();
+        }
+    }
+}
