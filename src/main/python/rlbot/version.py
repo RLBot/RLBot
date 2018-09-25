020893a5eeba5270634b472a28d590a278cd4953
Tyler <tarehart@gmail.com>
2018-09-25 00:50:03 -0700

    New dll with a fix for dodges. (#260)

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -3,9 +3,12 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 release_notes = {
+    '1.0.6': """
+    The latest Rocket League patch broke dodges for our bots; this update fixes it.
+    """,
     '1.0.5': """
     Maximum size for a render message has been decreased again because many people experienced
     errors related to memory access. The limit is now only double the original.
