587b04211357f588ea4c60cecfd6a09bd011426c
Tyler Arehart <tarehart@gmail.com>
2018-11-05 03:16:52 -0800

    Version notes for dropshot fieldinfo fix.

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -3,9 +3,13 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-__version__ = '1.2.5'
+__version__ = '1.2.6'
 
 release_notes = {
+    '1.2.6': """
+    Fixed a bug where field info was not extracted properly during dropshot mode.
+    It was reporting 2 goals rather than the expected 140.
+    """,
     '1.2.5': """
     ***************************************************
     *  Fix for dodge cancels / half flips! - ccman32  *
