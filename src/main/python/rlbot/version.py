ec90373b7b51b1a2cd15f56bf4f83dd6c9dafb5d
Tyler Arehart <tarehart@gmail.com>
2018-11-17 22:46:05 -0800

    Version notes for have_internet helper function.

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -3,9 +3,12 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-__version__ = '1.4.2'
+__version__ = '1.5.0'
 
 release_notes = {
+    '1.5.0': """
+    Adding a have_internet helper function to help streamline upgrade checks. - ima9rd
+    """,
     '1.4.2': """
     Adding support for auto-running java bots during tournaments. To take advantage of this
     in your bot, see https://github.com/RLBot/RLBotJavaExample/wiki/Auto-Launching-Java
