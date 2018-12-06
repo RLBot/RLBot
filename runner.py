c7ea3f3549bd5c34ca2224ab1cb3eced0d47c708
Dominik Schmid <dominikschmid93@gmail.com>
2018-12-06 08:26:05 -0800

    Add a Python version check in case the user is running an old version (#304)

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import sys
 import os.path
 sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.abspath(__file__)) + '/src/main/python/'))
+from rlbot.utils import python_version_check
 from rlbot import runner as framework_runner
 
 if __name__ == '__main__':
---
+++ src/main/python/rlbot/utils/python_version_check.py
@@ -0,0 +1,12 @@
+import sys
+
+# This module exists to give users an indication that they need to have
+# a version of python compatible with the RLBot framework.
+# Otherwise people might find out by in-the-guts error messages
+# after quite a while of the runner launching.
+minimum_python_version = (3, 6)
+
+# Deliberately using old string formatting for compatibility.
+error_string = "You appear to be using an old version of Python: %s\n RLBot requires Python %d.%d or later.\n After installing, ensure your environment point to the new Python version, then run setup.bat" % ((sys.version,) + minimum_python_version)
+
+assert sys.version_info >= minimum_python_version, error_string
