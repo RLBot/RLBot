920a3200a30f31c304122ff2c01729e4c113b991
Dominik Schmid <dominikschmid93@gmail.com>
2018-12-09 22:24:25 +1100

    Added recommended python version.

--- src/main/python/rlbot/utils/python_version_check.py
+++ src/main/python/rlbot/utils/python_version_check.py
@@ -5,8 +5,9 @@ import sys
 # Otherwise people might find out by in-the-guts error messages
 # after quite a while of the runner launching.
 minimum_python_version = (3, 6)
+recommended_python_version = (3, 7)
 
 # Deliberately using old string formatting for compatibility.
-error_string = "You appear to be using an old version of Python: %s\n RLBot requires Python %d.%d or later.\n After installing, ensure your environment point to the new Python version, then run setup.bat" % ((sys.version,) + minimum_python_version)
+error_string = "You appear to be using an old version of Python: %s\n RLBot requires Python %d.%d or later. Python %d.%d is recommended.\n After installing, ensure your environment point to the new Python version, then run setup.bat" % ((sys.version,) + minimum_python_version + recommended_python_version)
 
 assert sys.version_info >= minimum_python_version, error_string
