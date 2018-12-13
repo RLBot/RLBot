74c20207e22969b6a4f0b490fac58a0c108bfe07
Marvin <35970829+RLMarvin@users.noreply.github.com>
2018-12-13 05:34:02 +0000

    Support all subscriptable types.

--- src/main/python/rlbot/utils/rendering/rendering_manager.py
+++ src/main/python/rlbot/utils/rendering/rendering_manager.py
@@ -331,11 +331,20 @@ class RenderingManager:
                 x = vec[0].x
                 y = vec[0].y
                 z = vec[0].z
-            # support for chip's LinearAlgebra.vec3 and numpy arrays
-            elif type(vec[0]).__name__ == 'vec3' or type(vec[0]).__name__ == 'ndarray' and vec[0].shape == (3,):
-                x = vec[0][0]
-                y = vec[0][1]
-                z = vec[0][2]
+            elif hasattr(vec[0], "__getitem__"):  # Support all subscriptable types.
+                try:
+                    x = float(vec[0][0])
+                    y = float(vec[0][1])
+                    try:
+                        z = float(vec[0][2])
+                    except (ValueError, IndexError):
+                        z = 0
+                except ValueError:
+                    raise ValueError(
+                        "Unexpected type(s) for creating vector: {0}, {1}, {2}".format(
+                            type(vec[0][0]), type(vec[0][1]), type(vec[0][2])))
+                except IndexError:
+                    raise IndexError("Unexpected IndexError when creating vector from type: {0}".format(type(vec[0])))
             else:
                 raise ValueError("Unexpected type for creating vector: {0}".format(type(vec[0])))
         elif len(vec) == 2 or len(vec) == 3:
