8d283b86566146dab8fe1a54b818bc8fe5fabae1
Tyler Arehart <tarehart@gmail.com>
2018-12-18 23:43:29 -0800

    Reverting binaries and version notes because we have more stuff coming soon.

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -3,12 +3,9 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-__version__ = '1.6.6'
+__version__ = '1.6.5'
 
 release_notes = {
-    '1.6.6': """
-    Automatically limiting controller inputs to valid ranges so bots don't get paralyzed. - tarehart
-    """,
     '1.6.5': """
     Support all subscriptable types for rendering. - Marvin 
     """,
