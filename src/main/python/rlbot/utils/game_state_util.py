e62a7f3df40a2389580ce5ab10d512f41398a5a2
Dominik Schmid <dominikschmid93@gmail.com>
2018-12-24 21:30:37 +1300

    Refactored set_game_state to take a GameState

--- src/main/python/rlbot/utils/game_state_util.py
+++ src/main/python/rlbot/utils/game_state_util.py
@@ -1,7 +1,8 @@
 import flatbuffers
+from typing import Union
+
 from rlbot.messages.flat import Float, RotatorPartial, Vector3Partial, DesiredPhysics, DesiredGameState, \
     DesiredCarState, DesiredBallState, DesiredBoostState, DesiredGameInfoState, Bool
-
 from rlbot.utils.structures import game_data_struct
 from rlbot.messages.flat import GameTickPacket
 
@@ -142,7 +143,6 @@ class BoostState:
             DesiredBoostState.DesiredBoostStateAddRespawnTime(builder, Float.CreateFloat(builder, self.respawn_time))
         return DesiredBoostState.DesiredBoostStateEnd(builder)
 
-
 class GameInfoState:
 
     def __init__(self, world_gravity_z: float = None):
@@ -222,7 +222,7 @@ class GameState:
         return DesiredGameState.DesiredGameStateEnd(builder)
 
     @staticmethod
-    def create_from_gametickpacket(game_tick_packet):
+    def create_from_gametickpacket(game_tick_packet: Union[game_data_struct.GameTickPacket, GameTickPacket.GameTickPacket]):
         game_state = GameState()
         if isinstance(game_tick_packet, game_data_struct.GameTickPacket):
             car_states = {}
