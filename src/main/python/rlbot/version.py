39404c65e4a8f6dc613902b19bbb3bb10d583d02
Marvin <35970829+RLMarvin@users.noreply.github.com>
2018-12-31 04:13:40 +0000

    Update version notes

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -3,12 +3,13 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-__version__ = '1.8.4'
+__version__ = '1.9.0'
 
 release_notes = {
-    '1.8.4': """
+    '1.9.0': """
     - *Much* faster core dll initialization! - ccman32
     - Allow the user to change the appearance of human and party-member bot agents via the GUI - r0bbi3
+    - Added game speed info to game tick packet and the ability to modify it via state setting - Marvin
     """,
     '1.8.3': """
     - Allow SimpleControllerState initialization. - Marvin
