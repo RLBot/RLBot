0cf151d10a9823ef716dbaf8ffb5ef2ec5f0504a
Tyler Arehart <tarehart@gmail.com>
2018-12-30 22:01:18 -0500

    Version notes for 1.8.4.

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -3,9 +3,13 @@
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
-__version__ = '1.8.3'
+__version__ = '1.8.4'
 
 release_notes = {
+    '1.8.4': """
+    - *Much* faster core dll initialization! - ccman32
+    - Allow the user to change the appearance of human and party-member bot agents via the GUI - r0bbi3
+    """,
     '1.8.3': """
     - Allow SimpleControllerState initialization. - Marvin
     - Passing more params to subprocess agents. - whatisaphone
