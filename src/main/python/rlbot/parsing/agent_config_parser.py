dfe9a5b1097de6e959a9733e312ff26079b9d4b3
Tyler Arehart <tarehart@gmail.com>
2019-01-01 14:46:01 -0500

    Using bot config scanning in the gui.

--- src/main/python/rlbot/parsing/agent_config_parser.py
+++ src/main/python/rlbot/parsing/agent_config_parser.py
@@ -24,10 +24,12 @@ class BotConfigBundle:
     def __init__(self, config_directory, config_obj: ConfigObject, config_file_name: str = None):
         self.config_directory = config_directory
         self.config_file_name = config_file_name
+        self.config_path = os.path.join(self.config_directory, self.config_file_name)
         self.config_obj = config_obj
         self.base_agent_config = BaseAgent.base_create_agent_configurations()
         self.base_agent_config.parse_file(self.config_obj, config_directory=config_directory)
         self.name = config_obj.get(BOT_CONFIG_MODULE_HEADER, BOT_NAME_KEY)
+        self.looks_path = self.get_absolute_path(BOT_CONFIG_MODULE_HEADER, LOOKS_CONFIG_KEY)
 
     def get_absolute_path(self, header, key):
         path = self.base_agent_config.get(header, key)
@@ -75,14 +77,13 @@ def add_participant_header(config_object):
                                              Use None to extract the path from the agent config""")
 
 
-def get_looks_config(config_bundle):
+def get_looks_config(config_bundle) -> ConfigObject:
     """
     Creates a looks config from the config bundle
     :param config_bundle:
     :return:
     """
-    looks_path = config_bundle.get_absolute_path(BOT_CONFIG_MODULE_HEADER, LOOKS_CONFIG_KEY)
-    return BaseAgent._create_looks_configurations().parse_file(looks_path)
+    return BaseAgent._create_looks_configurations().parse_file(config_bundle.looks_path)
 
 
 def get_sanitized_bot_name(dict, name):
