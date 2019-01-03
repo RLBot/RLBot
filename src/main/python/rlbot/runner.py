9a89c4a73e1dc255fdcff305b41802304caa0356
Marvin <35970829+RLMarvin@users.noreply.github.com>
2019-01-03 17:54:54 +0000

    Move python version check to rlbot.runner

--- src/main/python/rlbot/runner.py
+++ src/main/python/rlbot/runner.py
@@ -1,9 +1,11 @@
 from rlbot.setup_manager import SetupManager
+from rlbot.utils.python_version_check import check_python_version
 
 
 def main():
 
     print("starting")
+    check_python_version()
     manager = SetupManager()
     manager.connect_to_game()
     manager.load_config()
