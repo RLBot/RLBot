fdfaf68ae2388fbb20283364c2b5240920742544
Tyler Arehart <tarehart@gmail.com>
2019-02-07 20:27:24 -0800

    Version notes.

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -4,11 +4,12 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '1.14.0'
+__version__ = '1.14.1'
 
 release_notes = {
-    '1.14.0': """
+    '1.14.1': """
     - Adding a way of starting matches using a flatbuffer message. - tarehart
+    - More accurate get_output call frequency for python bots. - Marvin and chip
     """,
 
     '1.13.2': """
