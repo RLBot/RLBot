b407362263692b5baa1022080c681868e7b8b797
Skyborg <fabiorcferreira@gmail.com>
2019-02-19 20:49:43 +0000

    bumping version

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -4,7 +4,7 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '1.14.4'
+__version__ = '1.14.5'
 
 release_notes = {
     '1.14.4': """
