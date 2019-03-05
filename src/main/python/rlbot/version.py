4a0583d0a8fc350604d44ca151cecc716cb7ad12
Tyler Arehart <tarehart@gmail.com>
2019-03-05 00:05:16 -0800

    Make the GUI allow non-vital cfg sections to be missing.

--- src/main/python/rlbot/version.py
+++ src/main/python/rlbot/version.py
@@ -4,10 +4,10 @@
 # 3) we can import it into your module module
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '1.14.8'
+__version__ = '1.14.9'
 
 release_notes = {
-    '1.14.8': """
+    '1.14.9': """
     - Adding a way of starting matches using a flatbuffer message. - tarehart
     - More accurate get_output call frequency for python bots. - Marvin and chip
     - Fixing compilation of RLBotDotNet project with a breaking change. - tarehart
@@ -15,6 +15,7 @@ release_notes = {
     - Fixed for the friends update. - ccman32
     - Fix for psyonix bots never using boost. - tarehart
     - Avoid killing the parent process, e.g. the GUI, when subprocess agents retire. - tarehart
+    - Make the GUI allow non-vital cfg sections to be missing. - tarehart
     """,
 
     '1.13.2': """
