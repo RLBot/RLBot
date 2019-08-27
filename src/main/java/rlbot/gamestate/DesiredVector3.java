7b13ae977666631d24ed663f94e0697f13ca1353
dtracers <dtracers@gmail.com>
2019-08-27 17:14:00 -0600

    Update DesiredVector3.java (#447)
    Refer to methods instead of raw variables.
    Useful in cases of subclassing
    
    Use has instead.
    Better for cases where get may not be static OR may be expensive to compute

--- src/main/java/rlbot/gamestate/DesiredVector3.java
+++ src/main/java/rlbot/gamestate/DesiredVector3.java
@@ -23,6 +23,13 @@ public class DesiredVector3 {
     public Float getX() {
         return x;
     }
+    
+    /**
+     * @returns {@code true} If x is not null.
+     */
+    public boolean hasX() {
+        return x != null
+    }
 
     public DesiredVector3 withX(Float x) {
         this.x = x;
@@ -32,6 +39,13 @@ public class DesiredVector3 {
     public Float getY() {
         return y;
     }
+    
+    /**
+     * @returns {@code true} If y is not null.
+     */
+    public boolean hasY() {
+        return y != null
+    }
 
     public DesiredVector3 withY(Float y) {
         this.y = y;
@@ -40,6 +54,13 @@ public class DesiredVector3 {
 
     public Float getZ() {
         return z;
+    }
+    
+    /**
+     * @returns {@code true} If z is not null.
+     */
+    public boolean hasZ() {
+        return z != null
     }
 
     public DesiredVector3 withZ(Float z) {
@@ -53,14 +74,14 @@ public class DesiredVector3 {
      */
     public int toFlatbuffer(FlatBufferBuilder builder) {
         Vector3Partial.startVector3Partial(builder);
-        if (x != null) {
-            Vector3Partial.addX(builder, rlbot.flat.Float.createFloat(builder, x));
+        if (hasX()) {
+            Vector3Partial.addX(builder, rlbot.flat.Float.createFloat(builder, getX()));
         }
-        if (y != null) {
-            Vector3Partial.addY(builder, rlbot.flat.Float.createFloat(builder, y));
+        if (hasY()) {
+            Vector3Partial.addY(builder, rlbot.flat.Float.createFloat(builder, getY()));
         }
-        if (z != null) {
-            Vector3Partial.addZ(builder, rlbot.flat.Float.createFloat(builder, z));
+        if (hasZ()) {
+            Vector3Partial.addZ(builder, rlbot.flat.Float.createFloat(builder, getZ()));
         }
         return Vector3Partial.endVector3Partial(builder);
     }
