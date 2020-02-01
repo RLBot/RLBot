ade125dac3122500bfc17cea4128c6d8f34c53af
Blocks <21197843+TheBlocks@users.noreply.github.com>
2020-02-01 21:32:44 +0100

    Adding more info to RLBotDotNet nuspec

--- src/main/cs/RLBotDotNet/RLBotDotNet/Properties/AssemblyInfo.cs
+++ src/main/cs/RLBotDotNet/RLBotDotNet/Properties/AssemblyInfo.cs
@@ -21,15 +21,10 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f97c012e-6c20-4e76-a075-6a274d73a69f")]
 
-// Version information for an assembly consists of the following four values:
+// RLBotDotNet version information for an assembly consists of the following three values:
 //
 //      Major Version
 //      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.12.0")]
-[assembly: AssemblyFileVersion("1.12.0")]
+//      Patch Number
+[assembly: AssemblyVersion("1.12.1")]
+[assembly: AssemblyFileVersion("1.12.1")]
