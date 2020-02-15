9248fc1aa81ee7993559f0f5d55e16258e80daba
Robbie <robbiealternate@gmail.com>
2020-02-15 13:56:54 +0000

    Correct spacing in Psyonix names

--- src/main/python/rlbot/matchconfig/psyonix_config.py
+++ src/main/python/rlbot/matchconfig/psyonix_config.py
@@ -16,4 +16,4 @@ def set_random_psyonix_bot_preset(player: PlayerConfig):
     loadout_config = create_looks_configurations().parse_file(loadout_file)
     player.loadout_config = load_bot_appearance(loadout_config, player.team)
     player.name = loadout_file.name.split("_")[1].title()
-    player.name = ["Rookie", "Pro", ""][min(int(abs(player.bot_skill) * 3), 2)] + " " + player.name
+    player.name = ["Rookie ", "Pro ", ""][min(int(abs(player.bot_skill) * 3), 2)] + player.name
