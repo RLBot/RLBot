2cf0abbc028d1be9b2a2d063e2088231074cc881
Robbie <42142350+robbai@users.noreply.github.com>
2020-02-15 21:04:34 +0000

    Don't use the same Psyonix preset multiple times (#479)

--- src/main/python/rlbot/matchconfig/psyonix_config.py
+++ src/main/python/rlbot/matchconfig/psyonix_config.py
@@ -6,14 +6,16 @@ from rlbot.parsing.agent_config_parser import load_bot_appearance, create_looks_
 
 PSYONIX_PRESET_DIRECTORY = Path(__file__).parent / "psyonix_presets"
 PSYONIX_PRESET_LIST = list(PSYONIX_PRESET_DIRECTORY.glob("*.cfg"))
+random.shuffle(PSYONIX_PRESET_LIST)
 
 
 def set_random_psyonix_bot_preset(player: PlayerConfig):
     """
     Sets the name and loadout to a randomly selected psyonix bot preset.
     """
-    loadout_file = random.choice(PSYONIX_PRESET_LIST)
+    loadout_file = PSYONIX_PRESET_LIST[0]
+    PSYONIX_PRESET_LIST.append(PSYONIX_PRESET_LIST.pop(0))  # Move preset to the end of the list.
     loadout_config = create_looks_configurations().parse_file(loadout_file)
     player.loadout_config = load_bot_appearance(loadout_config, player.team)
     player.name = loadout_file.name.split("_")[1].title()
-    player.name = ["Rookie", "Pro", ""][min(int(abs(player.bot_skill) * 3), 2)] + " " + player.name
+    player.name = ["Rookie ", "Pro ", ""][min(int(abs(player.bot_skill) * 3), 2)] + player.name
