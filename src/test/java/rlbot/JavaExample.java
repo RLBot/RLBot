19cda6a39acbb1338250f4eb2f53edb1dc2fd16b
Tyler Arehart <tarehart@gmail.com>
2020-03-22 11:29:13 -0700

    Removing the old py4j interface from the Java framework.

--- src/test/java/rlbot/JavaExample.java
+++ src/test/java/rlbot/JavaExample.java
@@ -1,8 +1,6 @@
 package rlbot;
 
 import rlbot.manager.BotManager;
-import rlbot.pyinterop.PythonInterface;
-import rlbot.pyinterop.PythonServer;
 import rlbot.pyinterop.SocketServer;
 
 /**
@@ -18,7 +16,7 @@ public class JavaExample {
 
         BotManager flatBotManager = new BotManager();
         flatBotManager.setRefreshRate(120);
-        Integer port = Integer.parseInt(args[0]);
+        int port = Integer.parseInt(args[0]);
         SocketServer server = new SamplePythonInterface(port, flatBotManager);
         server.start();
     }
